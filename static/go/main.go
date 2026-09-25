9ce6af7946a7dd8be88d20b260da963006804f66
Mohammad Javad <yellowmanhatami895@gmail.com>
2026-09-25 18:44:36 +0330

    bug - main.html = side va footer nemiad vaghti ba go baz mishan

--- static/go/main.go
+++ static/go/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
 )
 
 var db *sql.DB
 var err error
+var userID int
+var userRole string
+var userName string
+var userPassword string
 
 func main() {
 	db, err = sql.Open("sqlite", "../db/OnlineLibrary.db")
@@ -15,10 +22,45 @@ func main() {
 		return
 	}
 	defer db.Close()
+
 	createTables()
+
 	if DataBaseIsEmpty() {
 		fmt.Println("DataBase is Empty")
-		// record := getInputs([]string{"Enter first username", "Enter email", "Enter first password"})
-		//insertIntoUsers(append(record, "Admin"))
 	}
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("../css"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("../img"))))
+	http.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("../html"))))
+	http.HandleFunc("/Login", login)
+
+	fmt.Println(":8080")
+	http.ListenAndServe(":8000", nil)
+}
+func login(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+
+		id := r.FormValue("id")
+		userID, err = strconv.Atoi(id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		password := r.FormValue("password")
+
+		fmt.Println("id:", id)
+		fmt.Println("Password:", password)
+
+		db.QueryRow("SELECT name,password,role FROM users WHERE id = ?", userID).Scan(&userName, &userPassword, &userRole)
+		if password == userPassword {
+			template, err := template.ParseFiles("../html/main.html")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			template.Execute(w, userName)
+			return
+		}
+	}
+
+	http.ServeFile(w, r, "../html/Login.html")
 }
